2fd45d2547cd92aaacdad441bc4926eb664aecf8
peaceputter <70102333+peaceputter@users.noreply.github.com>
2026-03-24 13:17:45 +0530

    Update query_agent.py

--- agents/query_agent.py
+++ agents/query_agent.py
@@ -1,25 +1,25 @@
-import google.generativeai as genai
-from config import GEMINI_API_KEY
+#import google.generativeai as genai
+#from config import GEMINI_API_KEY
 
-genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
+#genai.configure(api_key=GEMINI_API_KEY)
+#model = genai.GenerativeModel("gemini-1.5-flash")
 
 def rewrite_query(user_query):
-    prompt = f"""
-You are an electronics engineer.
+#    prompt = f"""
+#You are an electronics engineer.
 
-Convert the user query into a precise component search query.
+#Convert the user query into a precise component search query.
 
-Examples:
-- "fast charging chip" → "USB PD controller 60W"
-- "gaming RAM" → "LPDDR5 6400 8GB"
+#Examples:
+#- "fast charging chip" → "USB PD controller 60W"
+#- "gaming RAM" → "LPDDR5 6400 8GB"
 
-User query: {user_query}
+#User query: {user_query}
 
-Return ONLY the improved search query.
-"""
+#Return ONLY the improved search query.
+#"""
 
-    try:
-        return model.generate_content(prompt).text.strip()
-    except:
+ #   try:
+ #       return model.generate_content(prompt).text.strip()
+ #   except:
         return user_query
