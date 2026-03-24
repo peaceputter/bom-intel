836cd40256994e85ad0aedd2874a5122dc735f4f
peaceputter <70102333+peaceputter@users.noreply.github.com>
2026-03-24 13:19:35 +0530

    Update ranking_agent.py

--- agents/ranking_agent.py
+++ agents/ranking_agent.py
@@ -1,31 +1,31 @@
-import google.generativeai as genai
-from config import GEMINI_API_KEY
+#import google.generativeai as genai
+#from config import GEMINI_API_KEY
 
-genai.configure(api_key=GEMINI_API_KEY)
+#genai.configure(api_key=GEMINI_API_KEY)
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+#model = genai.GenerativeModel("gemini-1.5-flash")
 
 def rank(query, components):
-    prompt = f"""
-You are an expert electronics sourcing engineer.
+#    prompt = f"""
+#You are an expert electronics sourcing engineer.
 
-User query: {query}
+#User query: {query}
 
-Components:
-{components}
+#Components:
+#{components}
 
-Rules:
-- Only use given components
-- Pick best 3
-- Explain why
-- Mention sourcing advantages
-- Be concise and practical
+#Rules:
+#- Only use given components
+#- Pick best 3
+#- Explain why
+#- Mention sourcing advantages
+#- Be concise and practical
 
-Output clearly.
-"""
+#Output clearly.
+#"""
 
-    try:
-        response = model.generate_content(prompt)
-        return response.text
-    except Exception as e:
-        return f"LLM Error: {e}"
+#    try:
+#        response = model.generate_content(prompt)
+        return "response_text"
+#    except Exception as e:
+#        return f"LLM Error: {e}"
